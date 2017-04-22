e409ffc80d55a23e462f67e2384c8e5085c95c47
iampeterkr <i.am.peter.kr@kaist.ac.kr>
2017-04-22 13:10:18 +0900

    models.py UrlMeta table 추가

--- ccp/models.py
+++ ccp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class UrlMeta(models.Model):
+
+    market = models.CharField(max_length=20)
+    product = models.CharField(max_length=20)
+    member = models.CharField(max_length=20)
+    process = models.CharField(max_length=20)
+    item = models.CharField(max_length=20)
+    seq = models.CharField(max_length=20)
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now_add=True)
+
+
+
