964645fe5a3fe25779888b129e6f8891a7efed07
iampeterkr <i.am.peter.kr@kaist.ac.kr>
2017-04-22 13:23:32 +0900

    20170422. models.py admin.py 추가

--- ccp/admin.py
+++ ccp/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import UrlMeta
+
+admin.site.register(UrlMeta)
+admin.site.unregister(UrlMeta)
+
 
 # Register your models here.
+
+@admin.register(UrlMeta)
+class UrlMetaAdmin(admin.ModelAdmin):
+    pass
+
