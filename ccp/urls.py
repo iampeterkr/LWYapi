bcf32d67869ec293c897c96e457edb690d812dd0
iampeterkr <i.am.peter.kr@kaist.ac.kr>
2017-04-22 12:28:12 +0900

    20170422.commit

--- ccp/urls.py
+++ ccp/urls.py
@@ -5,11 +5,11 @@ from . import views
 
 urlpatterns = [
     # [\w/\-/]+ 영어대소문자 숫자 '-', \w/{5} 5자리
-    url(r'^(?P<product>[\w/\-/]+)/(?P<member>[\w/]{5})/(?P<process>[\w/]+)/(?P<item>[\w/]+)/(?P<seq>[\d]+)/$', views.ListView) ,
-    url(r'^(?P<product>[\w/\-/]+)/(?P<member>[\w/]{5})/(?P<process>[\w/]+)/(?P<item>[\w/]+)/$', views.ListView) ,
-    url(r'^(?P<product>[\w/\-/]+)/(?P<member>[\w/]{5})/(?P<process>[\w/]+)/$', views.ListView) ,
-    url(r'^(?P<product>[\w/\-/]+)/(?P<member>[\w/]{5})/$', views.ListView) ,
-    url(r'^(?P<product>[\w/\-/]+)/$', views.ListView) ,
+    url(r'^(?P<product>[\w/\-/]+)/(?P<member>[\w/]{5})/(?P<process>[\w/]+)/(?P<item>[\w/]+)/(?P<seq>[\d]+)/$', views.MainView) ,
+    url(r'^(?P<product>[\w/\-/]+)/(?P<member>[\w/]{5})/(?P<process>[\w/]+)/(?P<item>[\w/]+)/$', views.MainView) ,
+    url(r'^(?P<product>[\w/\-/]+)/(?P<member>[\w/]{5})/(?P<process>[\w/]+)/$', views.MainView) ,
+    url(r'^(?P<product>[\w/\-/]+)/(?P<member>[\w/]{5})/$', views.MainView) ,
+    url(r'^(?P<product>[\w/\-/]+)/$', views.MainView) ,
 
     #url(r'^(?P<product>[a-zA-Zㄱ-힣/\d/]+)/(?P<member>[\d/]+)/$' , views.ListView) ,
     #url(r'^')
--- ccp/views.py
+++ ccp/views.py
@@ -4,18 +4,57 @@ from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
 
 from LWYapi import settings
+from . import constant
 
 #global i_process
 
+def MainView(request, product="", member="", process="", item="", seq=""):
 
-def ListView(request, product="", member="", process="", item="", seq=""):
+    # call the MainView()
+    rtUrlCheck = UrlCheckView(product, member, process, item, seq)
 
-#    print("request : ", product, member, process, item, seq)
+    print(rtUrlCheck)
+    if rtUrlCheck :
+        pass
+
+
+    print("URL_PROCESS :", constant.URL_ITEM_LIST)
+
+    print("Main/process : ", process)
+    # Process 별 함수 call
+    if process == "list" or process == "LIST":
+        result = "list 호출 하셨습니다"
+        RtList = ListView()
+        result += RtList
+
+    elif process == "data" or process == "DATA":
+        result = "data 호출 하셨습니다"
+        RtData = DataView()
+        result += RtData
+    else :
+        return HttpResponse("Check the process(:{}) ".format(process))
+
+
+    result += "안녕하세요 ListView 클래스's " \
+              " - Product : {} " \
+              " - member  : {} " \
+              " - process : {} " \
+              " - item    : {} " \
+              " - seq     : {}" \
+        .format(product , member , process , item , seq)
+
+    return HttpResponse(result)
+
+
+# def UrlCheckView(product="", member="", process="", item="", seq=""):
+def UrlCheckView(product, member, process, item, seq):
+
+    print("UrlCheckView : ", product, member, process, item, seq)
 
     if product > "" :
         pass
     else :
-        return HttpResponse(" check the product ")
+        return "CHECK_PRODUCT"
 
     if member > "" :
         pass
@@ -35,34 +74,26 @@ def ListView(request, product="", member="", process="", item="", seq=""):
     if seq > "" :
         pass
     else :
-        if process == "LIST" :
+        if process == "LIST"  or process == "list":
+            print("check seq-list")
             pass
         else :
-            return HttpResponse(" check the seq ")
+            return HttpResponse(" check the seq: need the seq at process : DATA ")
+
+    return True
 
 
 
+def ListView():
 
-# Process 별 함수 call
-    if process == "list" :
-        result = "list 호출 하셨습니다"
-    elif process == "data" :
-        result = "data 호출 하셨습니다"
-    else :
-        return HttpResponse("Check the process(:{}) ".format(process))
+    #print("- Called the ListView() ")
+    return (" * Called the ListView() ")
 
 
+def DataView():
 
-    result += "안녕하세요 ListView 클래스's "\
-             " - Product : {} " \
-             " - member  : {} " \
-             " - process : {} " \
-             " - item    : {} " \
-             " - seq     : {}" \
-             .format(product , member , process , item , seq) \
-
-    return HttpResponse(result)
-
+    #print("- Called the DataView() ")
+    return (" * Called the DataView() ")
 
 
 
