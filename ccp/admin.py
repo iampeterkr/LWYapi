45b2b374eb77a253f7a095eb1c3a8e8463041d91
iampeterkr <i.am.peter.kr@kaist.ac.kr>
2017-04-24 19:39:15 +0900

    models.py 회원정보DB테이블생성 및 admin 추가

--- ccp/admin.py
+++ ccp/admin.py
@@ -1,11 +1,21 @@
 from django.contrib import admin
-from .models import UrlMeta
+from .models import UrlMeta, MemberInfo
 
 admin.site.register(UrlMeta)
 admin.site.unregister(UrlMeta)
 
+admin.site.register(MemberInfo)
+admin.site.unregister(MemberInfo)
+
 
 # Register your models here.
+
+@admin.register(MemberInfo)
+class MemberInfoAdmin(admin.ModelAdmin):
+    list_display = ['market', 'member', 'bic_code', 'lei_code',
+                    'member_name','login_id', 'login_pass',
+                    'irs_won', 'irs_usd', 'ndf',
+                    'created_at', 'updated_at']
 
 @admin.register(UrlMeta)
 class UrlMetaAdmin(admin.ModelAdmin):
--- ccp/models.py
+++ ccp/models.py
@@ -3,6 +3,30 @@ from django.db import models
 
 # Create your models here.
 
+# 회원정보 작성
+class MemberInfo(models.Model):
+    PRODUCT_CHOICE = (
+        ('y' , 'YES') ,
+        ('n' , 'NO') ,
+    )
+
+    market = models.CharField(max_length=20)
+    member = models.CharField(max_length=20)
+    bic_code = models.CharField(max_length=20)
+    lei_code = models.CharField(max_length=20)
+    member_name = models.CharField(max_length=20)
+    login_id = models.CharField(max_length=20)
+    login_pass = models.CharField(max_length=20)
+    irs_won = models.CharField(max_length=1, choices=PRODUCT_CHOICE)
+    irs_usd = models.CharField(max_length=1, choices=PRODUCT_CHOICE)
+    ndf = models.CharField(max_length=1, choices=PRODUCT_CHOICE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+
+
+
+# URL에 사용할 Meta 정보를 DB로 만듬
 class UrlMeta(models.Model):
     MARKET_CHOICE = (
         ('c', 'CCP'),
