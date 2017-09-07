09adbfb10ae59c53cfef682cdbeb08b0a239772d
iampeterkr <i.am.peter.kr@kaist.ac.kr>
2017-09-07 16:25:59 +0900

    Channel websocke 사용

--- LWYapi/routing.py
+++ LWYapi/routing.py
@@ -1,11 +1,18 @@
 # routing.py
 from channels.routing import route
+# app 밑에 consumers 사용
+from chat.consumers import ws_message
 
 # channel_routing = [
-#     route('websocket.receive', 'chat.consumers.ws_echo'),
+
 # ]
 
 channel_routing = [
     # chat = app name
-    route("http.request", "chat.consumers.http_consumer"),
+    #route("http.request", "chat.consumers.http_consumer"),
+
+    # websocket 사용시
+    route("websocket.receive", ws_message),
+    # 아래와 같이 import 하지 않고 chat의 consumers의 ws_echo 함수를 사용한다고 명시할수있다
+    #route('websocket.receive', 'chat.consumers.ws_echo'),
 ]
